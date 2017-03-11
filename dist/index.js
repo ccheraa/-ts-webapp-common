409d58ff4f100ad3f9876331acdd390aef7b93c5
Ashraf Cheraa <ccheraa@gmail.com>
2017-03-11 20:04:54 +0100

    array polyfill not working. removed.

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,5 @@
 function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
-require("jspolyfill-array.prototype.findIndex");
 __export(require("./class"));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
-import 'jspolyfill-array.prototype.findIndex';
 export * from './db';
 export * from './class';
